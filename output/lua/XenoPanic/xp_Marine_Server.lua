cf74cf1a987b2a7802bdd4ee24577471852e8b8d
DaanVanYperen <daan@mostlyoriginal.net>
2014-10-12 18:44:20 +0200

    All marines always have at least an axe.

--- output/lua/XenoPanic/xp_Marine_Server.lua
+++ output/lua/XenoPanic/xp_Marine_Server.lua
@@ -2,11 +2,14 @@
 function Marine:InitWeapons()
 
     Player.InitWeapons(self)
+
+    self:GiveItem(Axe.kMapName)
+    self:SetQuickSwitchTarget(Axe.kMapName)
+    self:SetActiveWeapon(Axe.kMapName) 
     
     /* players spawn with zero weapons.
     self:GiveItem(Rifle.kMapName)
     self:GiveItem(Pistol.kMapName)
-    self:GiveItem(Axe.kMapName)
     self:GiveItem(Builder.kMapName)
     
     self:SetQuickSwitchTarget(Pistol.kMapName)
