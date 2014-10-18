e0178e6f5f581474ab3e7886479c518673a607f3
DaanVanYperen <daan@mostlyoriginal.net>
2014-10-18 17:55:24 +0200

    Add panic button (= Buy button).

--- output/lua/XenoPanic/xp_Shared.lua
+++ output/lua/XenoPanic/xp_Shared.lua
@@ -4,6 +4,8 @@ Script.Load("lua/XenoPanic/xp_Locale.lua")
 Script.Load("lua/XenoPanic/xp_Utility.lua")
 Script.Load("lua/XenoPanic/xp_EventMessenger.lua")
 
+Script.Load("lua/PanicMixin.lua")
+
 // Entity Overrides
 Script.Load("lua/XenoPanic/xp_Gamerules.lua")
 Script.Load("lua/XenoPanic/xp_PickupSpawning.lua")
