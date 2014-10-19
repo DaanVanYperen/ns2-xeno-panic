07be51e2dc5f0e0fe1330eac64c5671248f133ca
DaanVanYperen <daan@mostlyoriginal.net>
2014-10-19 14:29:05 +0200

    Version bump 0.0.4

--- output/lua/XenoPanic/xp_Client.lua
+++ output/lua/XenoPanic/xp_Client.lua
@@ -1,4 +1,4 @@
 decoda_name = "Client"
 Script.Load("lua/PreLoadMod.lua")
-Shared.Message('Xeno Panic [ALPHA/WIP] V0.0.3')
+Shared.Message('Xeno Panic [ALPHA/WIP] V0.0.4')
 Script.Load("lua/PostLoadMod.lua") 
